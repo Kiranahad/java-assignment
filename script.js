13bb4499e4b7c8d00f0207c70ae9b812239b7153
kiranahad <kiranahad1977@gmail.com>
2024-08-24 16:37:21 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,74 @@
+let a = 5;
+let b = 3;
+console.log("a = 5" , "b = 3",);
+console.log("a + b =", a + b,);
+console.log("a - b=",a - b,);
+
+let x = 5;
+let y = 2;
+console.log("x = 5",);
+console.log("y = 2",);
+console.log("x * Y =", x * y,);
+console.log("x / y =", x / y,);
+//modulus
+// let c = 4;
+// let d = 2; 
+// console.log(c%d);
+// //exponnentiation
+// console.log(x**)
+//comparision
+
+let c = 4;
+let d = 2; 
+console.log("x ==y",x == y,);//false(booleon)== equal
+console.log("x != y", x != y,);//true(!= not equal)
+let cond1 = x > d;
+let cond2 = x == d;
+console.log("cond1 && cond2 =", cond1 && cond2,);
+
+//CNIC QUESTION:
+// let name= prompt("Enter your name");
+// console.log("",name);
+// let age = prompt("Enter your age");
+// if(age > 17){//
+//     console.log("Eligible for CNIC");
+// }
+// else {
+//     console.log("Not eligible for CNIC");
+// }
+// //Even Or Odd Number;
+// let num=prompt("enter a number");
+// if (num % 5 === 0){
+//     num = "even" ;
+//   }
+//   else {
+//      num = "odd ";
+//  } 
+// console.log(num);  
+//positive or Negative
+// let number = prompt("Enter a number");
+//  if(number > 0){
+//          console.log("positive");
+//  }
+//  else{
+//    console.log("negative");
+//  }
+//Q5 
+let hours = parseInt(prompt("enter the current hours"));
+
+if (hours >= 5 && hours <12){
+  console.log("Good morning");
+}
+else if (hours >= 12 && hours  <16){
+console.log("good afternoon");
+}
+else if (hours >= 16 && hours  <=23){
+  console.log("good evening");
+  }
+  else if (hours >= 0 && hours  <5){
+    console.log("good night");
+    }
+  
+
+
+
